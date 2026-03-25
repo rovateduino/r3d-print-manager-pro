084eb43c19a5a4b3f26f82fd49fd0806517127de
rovateduino <97526529+rovateduino@users.noreply.github.com>
2026-03-25 19:17:35 -0400

    Update index.ts
    zxa

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { initializeApp, getApps, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
-import firebaseConfig from '../firebase-applet-config.json';
+import firebaseConfig from '../firebase-applet-config.json' assert { type: 'json' };
 
 // --- INICIALIZAÇÃO CORRIGIDA ---
 if (!getApps().length) {
